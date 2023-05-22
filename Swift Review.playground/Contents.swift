1c30d9699cf4e49251fd7c627437bebac1e23377
Mitchell <soggybag@gmail.com>
2023-05-22 08:09:58 -0700

    Initial commit

---
+++ Swift Review.playground/Contents.swift
@@ -0,0 +1,102 @@
+import UIKit
+
+//1. Declare a variable named color of type String, use type annotation.
+
+
+//2. Assign the value of "purple" to color
+
+
+//3. Change the value to "red"
+
+
+//4. Declare a constant named font of type String, use type annotation.
+
+
+//5. Assign the value of "Helvetica" to font
+
+
+//6. Change the value to "Arial"
+
+
+/*
+What's the difference between var and let?
+How would this exercise look if you used type inference instead of type annotation?
+*/
+
+
+// -----------------
+
+//1. Write a function that takes in a color (String) and prints out "My favorite color is _____"
+
+
+//2. Call your function
+
+
+//3. Write a function that takes in a color (String) and returns "My favorite color is _____"
+
+
+//4. Call the function, can you see the result?
+
+//5. Write a function that takes in a color (String) and a name (String) and returns "_____'s favorite color is _____"
+
+
+//6. Call the function, can you see the result?
+
+
+/*
+Did you use internal and external parameter names? If not, try it :)
+*/
+
+
+// ------------------------
+
+//Write a function called "isEven" that takes in an Int and returns a boolean to specify if the input is an even number.
+
+
+// --------------------------
+
+//Declare a variable called "color" of type optional String
+
+
+//What values can color store?
+//color = "lilac"
+
+
+//How would you print out the value of "color"?
+
+
+/*
+Review:
+- force unwrapping
+- optional binding
+- guard
+*/
+
+
+// ------------------------
+
+//1. Define an array called "fruits" and add fruit names.
+
+
+//2. Print how many elements are stored in the array
+
+
+//3. Print the second element in the array
+
+
+//4. Remove the first element in the array
+
+
+//5. Add another element to the array
+
+
+//6. Empty array
+
+
+/*
+Look into:
+- Creating an empty array
+- Built in methods (like sort)
+*/
+
+
